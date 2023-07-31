c9ad46f37bb0ad7d9270b32b83f647c7235a0d81
Aman B Singh <amanb.singh21@gmail.com>
2023-07-31 16:56:25 +0000

    buildah pipeline setup

--- terraform/buildah/main.tf
+++ terraform/buildah/main.tf
@@ -2,37 +2,29 @@ provider "aws" {
   region = "ap-south-1" # change to your AWS region
 }
 
-resource "aws_instance" "jenkins" {
+resource "aws_instance" "buildah" {
   ami           = "ami-0f5ee92e2d63afc18" # replace with the correct AMI ID for Jenkins
-  instance_type = "t2.medium"              # adjust as necessary
+  instance_type = "t2.micro"              # adjust as necessary
 
   key_name = "cicd-pipeline-ec2-key" # replace with your key pair name
 
-  vpc_security_group_ids = [aws_security_group.jenkins.id]
+  vpc_security_group_ids = [aws_security_group.buildah.id]
 
   associate_public_ip_address = true
 
   tags = {
-    Name = "jenkins-server"
+    Name = "buildah-server"
   }
 }
 
-resource "aws_security_group" "jenkins" {
-  name        = "jenkins-sg"
+resource "aws_security_group" "buildah" {
+  name        = "buildah-sg"
   description = "Allow inbound traffic"
 
   ingress {
     description = "SSH from anywhere"
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Jenkins"
-    from_port   = 8080
-    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
